3f09bab901e369cbb72455368524317ae23569d5
AtharvInamdar <90698481+AtharvInamdar@users.noreply.github.com>
2022-09-22 19:36:52 +0530

    Add files via upload

---
+++ practical3/dyanamicvecadd.c
@@ -0,0 +1,30 @@
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    clock_t begin = clock();
+    int n = 200000, i ,j=42;
+    int arr1[n], answer[n];
+    for(i = 0; i < n; i++){
+        arr1[i] = rand()%100;
+    }    
+    #pragma omp parallel for schedule(dynamic,20) shared(arr1, answer,j) private(i)
+    for(i = 0; i < n; i++)
+    {
+        answer[i] = arr1[i] + j;
+    }
+    printf("\nArray 1: \n");
+    for(i = 0; i < n; i++){
+        printf("\t %d", arr1[i]);
+    }
+    printf("\nAnswer: \n");
+    for(i = 0; i < n; i++){
+        printf("\t %d", answer[i]);
+    }
+    clock_t end = clock();
+    double time_execution = (double)(end-begin)/CLOCKS_PER_SEC;
+    printf("\nTime for execution: %lf", time_execution);
+    return 0;
+}
